89d5e602cbace486d0392fb903ac1c00d027fd44
scottasa <96374583+scottasa@users.noreply.github.com>
2022-05-04 18:47:23 -0700

    Add files via upload

---
+++ back.php
@@ -0,0 +1,26 @@
+<?php
+$name = $_POST['name'];
+$visitor_email = $_POST['email'];
+$subject = $_POST['subject'];
+$message = $_POST['message'];
+
+$email_from = 'info@pets.com';
+
+$email_subject = 'New form submission';
+
+$email_body = "User Name: $name.\n".
+                "User Email: $visitor_email.\n".
+                "Subject: $subject.\n".
+                "User Message: $message.\n";
+
+$to = 'scottasa2002@gmail.com';
+
+$headers = "from: $email_from \r\n";
+
+$headers .= "Reply-To: $visitor_email \r\n";
+
+
+mail($to,$email_subject,$email_body,$headers);
+
+header("Location: contact.html");
+?>
